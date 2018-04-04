6e96cf1d19e7c62dc88241c353f34f89753a0dc6
Ole Henrik Stabell <hennamann9999@gmail.com>
2018-04-04 10:18:41 +0200

    work on gradle scripts

--- src/main/java/com/henrikstabell/fogworld/FogWorld.java
+++ src/main/java/com/henrikstabell/fogworld/FogWorld.java
@@ -9,10 +9,10 @@ import static com.henrikstabell.fogworld.FogWorld.VERSION;
  * See The repos LICENSE.MD file for what you can and can't do with the code.
  * Created by Hennamann(Ole Henrik Stabell) on 03/04/2018.
  */
-@Mod(modid = MODID, version = VERSION)
+@Mod(modid = MODID, version = VERSION, certificateFingerprint = "@FINGERPRINT@")
 public class FogWorld {
 
     public static final String MODID = "fogworld";
-    public static final String VERSION = "@VERSION";
+    public static final String VERSION = "@VERSION@";
 
 }
