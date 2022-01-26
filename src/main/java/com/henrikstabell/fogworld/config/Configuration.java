fd2e53d46123715ddd12aeedc5634a3f2d8cde7f
Ole Henrik Stabell <ole@henrikstabell.com>
2022-01-26 21:31:15 +0100

    Added maven publishing to build.gradle

--- src/main/java/com/henrikstabell/fogworld/config/Configuration.java
+++ src/main/java/com/henrikstabell/fogworld/config/Configuration.java
@@ -1,7 +1,8 @@
 package com.henrikstabell.fogworld.config;
 
 import net.minecraftforge.common.ForgeConfigSpec;
-import net.minecraftforge.common.ForgeConfigSpec.*;
+import net.minecraftforge.common.ForgeConfigSpec.BooleanValue;
+import net.minecraftforge.common.ForgeConfigSpec.IntValue;
 import org.apache.commons.lang3.tuple.Pair;
 
 /**
@@ -10,75 +11,12 @@ import org.apache.commons.lang3.tuple.Pair;
  */
 public class Configuration {
 
-    public static class Client {
-
-        public final BooleanValue fogEnabled;
-        public final IntValue fogDensity;
-        public final IntValue fogColorRed;
-        public final IntValue fogColorGreen;
-        public final IntValue fogColorBlue;
-        public final BooleanValue incompatibleModsWarning;
-
-        public Client(ForgeConfigSpec.Builder builder) {
-            fogEnabled = builder
-                    .comment("Disables/Enables the fog rendering introduced by fog world. Vanilla fog is not disabled with this option")
-                    .translation("config.client.fogworld.fogenabled")
-                    .define("fogEnabled", true);
-            fogDensity = builder
-                    .comment("Controls the amount/density of the fog.")
-                    .translation("config.client.fogworld.fogdensity")
-                    .defineInRange("fogDensity", 3, 1, 600);
-            fogColorRed = builder
-                    .comment("Set the RED color value for the fog. Minimum 0, maximum 255")
-                    .translation("config.client.fogworld.fogcolorred")
-                    .defineInRange("fogColorRed", 0, 0, 255);
-            fogColorGreen = builder
-                    .comment("Set the GREEN color value for the fog. Minimum 0, maximum 255")
-                    .translation("config.client.fogworld.fogcolorgreen")
-                    .defineInRange("fogColorGreen", 0, 0, 255);
-            fogColorBlue = builder
-                    .comment("Set the BLUE color value for the fog. Minimum 0, maximum 255")
-                    .translation("config.client.fogworld.fogcolorblue")
-                    .defineInRange("fogColorBlue", 0, 0, 255);
-            incompatibleModsWarning = builder
-                    .comment("Enable warnings on load when using incompatible mods")
-                    .translation("config.client.fogworld.incompatiblemodswarning")
-                    .define("incompatibleModsWarning", true);
-        }
-
-    }
-
-    public static class Common {
-
-        public final IntValue poisonTicks;
-        public final IntValue poisonDamage;
-        public final BooleanValue poisonousFog;
-
-        public Common(ForgeConfigSpec.Builder builder) {
-            poisonousFog = builder
-                    .comment("Should the fog be poisonous?")
-                    .translation("config.common.fogworld.poisonousFog")
-                    .define("poisonousFog", false);
-            poisonTicks = builder
-                    .comment("How many ticks before the player takes damage in the fog? Minimum 20 (1 second), maximum 72000 (1 hour)")
-                    .translation("config.common.fogworld.poisonticks")
-                    .defineInRange("poisonTicks", 1200, 20, 72000);
-            poisonDamage = builder
-                    .comment("How much damage should the poison fog deal per tick? Minimum 1, Maximum 20")
-                    .translation("config.common.fogworld.poisondamage")
-                    .defineInRange("poisonDamage", 1, 1, 20);
-        }
-
-    }
-
     public static final ForgeConfigSpec clientSpec;
     public static final Client CLIENT;
-
     public static final ForgeConfigSpec commonSpec;
     public static final Common COMMON;
 
-    static
-    {
+    static {
         final Pair<Client, ForgeConfigSpec> clientSpecPair = new ForgeConfigSpec.Builder().configure(Client::new);
         clientSpec = clientSpecPair.getRight();
         CLIENT = clientSpecPair.getLeft();
@@ -122,6 +60,65 @@ public class Configuration {
 
     public static int poisonDamage() {
         return COMMON.poisonDamage.get();
+    }
+
+    public static class Client {
+
+        public final BooleanValue fogEnabled;
+        public final IntValue fogDensity;
+        public final IntValue fogColorRed;
+        public final IntValue fogColorGreen;
+        public final IntValue fogColorBlue;
+        public final BooleanValue incompatibleModsWarning;
+
+        public Client(ForgeConfigSpec.Builder builder) {
+            fogEnabled = builder
+                    .comment("Disables/Enables the fog rendering introduced by fog world. Vanilla fog is not disabled with this option")
+                    .translation("config.client.fogworld.fogenabled")
+                    .define("fogEnabled", true);
+            fogDensity = builder
+                    .comment("Controls the amount/density of the fog.")
+                    .translation("config.client.fogworld.fogdensity")
+                    .defineInRange("fogDensity", 3, 1, 600);
+            fogColorRed = builder
+                    .comment("Set the RED color value for the fog. Minimum 0, maximum 255")
+                    .translation("config.client.fogworld.fogcolorred")
+                    .defineInRange("fogColorRed", 0, 0, 255);
+            fogColorGreen = builder
+                    .comment("Set the GREEN color value for the fog. Minimum 0, maximum 255")
+                    .translation("config.client.fogworld.fogcolorgreen")
+                    .defineInRange("fogColorGreen", 0, 0, 255);
+            fogColorBlue = builder
+                    .comment("Set the BLUE color value for the fog. Minimum 0, maximum 255")
+                    .translation("config.client.fogworld.fogcolorblue")
+                    .defineInRange("fogColorBlue", 0, 0, 255);
+            incompatibleModsWarning = builder
+                    .comment("Enable warnings on load when using incompatible mods")
+                    .translation("config.client.fogworld.incompatiblemodswarning")
+                    .define("incompatibleModsWarning", true);
+        }
+    }
+
+    public static class Common {
+
+        public final IntValue poisonTicks;
+        public final IntValue poisonDamage;
+        public final BooleanValue poisonousFog;
+
+        public Common(ForgeConfigSpec.Builder builder) {
+            poisonousFog = builder
+                    .comment("Should the fog be poisonous?")
+                    .translation("config.common.fogworld.poisonousFog")
+                    .define("poisonousFog", false);
+            poisonTicks = builder
+                    .comment("How many ticks before the player takes damage in the fog? Minimum 20 (1 second), maximum 72000 (1 hour)")
+                    .translation("config.common.fogworld.poisonticks")
+                    .defineInRange("poisonTicks", 1200, 20, 72000);
+            poisonDamage = builder
+                    .comment("How much damage should the poison fog deal per tick? Minimum 1, Maximum 20")
+                    .translation("config.common.fogworld.poisondamage")
+                    .defineInRange("poisonDamage", 1, 1, 20);
+        }
     }
 
 }
